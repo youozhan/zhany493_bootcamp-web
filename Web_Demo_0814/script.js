3f84bba5f644b11508c677d11c82f55def898273
Youchun Zhang <zhany493@newschool.edu>
2017-08-14 15:45:26 -0400

    File verison change

--- Web_Demo_0814/script.js
+++ Web_Demo_0814/script.js
@@ -1,5 +1,5 @@
-var w = 400;
-var h = 400;
+var w = 400,
+    h = 400;
 
 var circleWidth = 5;
 
@@ -22,58 +22,58 @@ var palette = {
     "blue": "#2176C7",
     "green": "#259286",
     "yellowgreen": "#738A05"
-};
+}
 
 var nodes = [
-	{name: "Parent"},
-	{name: "child1"},
-	{name: "child2", target: [0]},
-	{name: "child3", target: [0]},
-	{name: "child4", target: [1]},
-	{name: "child5", target: [0, 1, 2, 3]}
-]
+    { name: "Parent" },
+    { name: "child1" },
+    { name: "child2", target: [0] },
+    { name: "child3", target: [0] },
+    { name: "child4", target: [1] },
+    { name: "child5", target: [0, 1, 2, 3] }
+];
 
 var links = [];
 
-for (var i = 0; i<nodes.length; i++) {
-	if (nodes[i].target !== undefined) {
-		for (var x = 0; x < nodes[i].target.length; x++)
-			links.push({
-				source: nodes[i],
-				target: nodes[nodes[i].target[x]]
-			})
-	}
+for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].target !== undefined) {
+        for (var x = 0; x < nodes[i].target.length; x++) {
+            links.push({
+                source: nodes[i],
+                target: nodes[nodes[i].target[x]]
+            })
+        }
+    }
 }
 
-var myChart = d3.select ('#chart')
-	.append('svg')
-	.attr('width', w)
-	.attr('height',h)
+var myChart = d3.select('#chart')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
 
 var force = d3.layout.force()
-	.nodes(nodes)
-	.links([])
-	.gravity(0.3)
-	.charge(-1000)
-	.size([w, h])
+    .nodes(nodes)
+    .links([])
+    .gravity(0.3)
+    .charge(-1000)
+    .size([w, h])
 
 var link = myChart.selectAll('line')
-	.data(links).enter().append('line')
-	.attr('stroke', palette.gray)
+    .data(links).enter().append('line')
+    .attr('stroke', palette.gray)
 
 var node = myChart.selectAll('circle')
-	.data(nodes).enter()
-	.append('g')
-	.call(force.drag);
+    .data(nodes).enter()
+    .append('g')
+    .call(force.drag);
 
 node.append('circle')
-	.attr('cx', function(d){return d.x;})
-	.attr('cy', function(d){return d.y;})
-	.attr('r', circleWidth)
-	.attr('fill', function(d, i) {
-		if(i>0) {return palette.darkblue}
-			else {return palette.blue}
-	})
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', circleWidth)
+    .attr('fill', function(d, i) {
+        if (i > 0) { return palette.pink } else { return palette.blue }
+    })
 
 node.append('text')
     .text(function(d) { return d.name })
@@ -95,15 +95,16 @@ node.append('text')
     })
 
 force.on('tick', function(e) {
-	node.attr('transform', function(d, i) {
-		return 'translate('+ d.x + ',' + d.y + ')';
-	})
+    node.attr('transform', function(d, i) {
+        return 'translate(' + d.x + ', ' + d.y + ')';
+    })
 
-	link
-		.attr('x1', function(d) {return d.source.x})
-		.attr('y1', function(d) {return d.source.y})
-		.attr('x2', function(d) {return d.source.x})
-		.attr('y2', function(d) {return d.source.y})
+    link
+        .attr('x1', function(d) { return d.source.x })
+        .attr('y1', function(d) { return d.source.y })
+        .attr('x2', function(d) { return d.target.x })
+        .attr('y2', function(d) { return d.target.y })
 })
+
 
 force.start();
